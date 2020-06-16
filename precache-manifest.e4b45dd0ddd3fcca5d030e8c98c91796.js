acfaeb14343c7515fe57f8690c71f1e659f75130
Josh Hooker <joshualeehooker@gmail.com>
2020-06-16 16:07:01 -0400

    Updates

---
+++ precache-manifest.e4b45dd0ddd3fcca5d030e8c98c91796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ea6c923574117092957b652c856349",
+    "url": "/Nuclear_Kinematics_Calculator/index.html"
+  },
+  {
+    "revision": "a2c6f30bbceb299eef60",
+    "url": "/Nuclear_Kinematics_Calculator/static/css/main.4e27fed4.chunk.css"
+  },
+  {
+    "revision": "08fe1d12b072dfa0418a",
+    "url": "/Nuclear_Kinematics_Calculator/static/js/2.09f4a9b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nuclear_Kinematics_Calculator/static/js/2.09f4a9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c6f30bbceb299eef60",
+    "url": "/Nuclear_Kinematics_Calculator/static/js/main.dfce3b69.chunk.js"
+  },
+  {
+    "revision": "e8c408fad45846118e1d",
+    "url": "/Nuclear_Kinematics_Calculator/static/js/runtime-main.2802df4c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Nuclear_Kinematics_Calculator/precache-manifest.e4b45dd0ddd3fcca5d030e8c98c91796.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nuclear_Kinematics_Calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
